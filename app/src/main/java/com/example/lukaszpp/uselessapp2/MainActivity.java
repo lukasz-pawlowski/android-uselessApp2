f5bbd19d3aa0907019c60722cd58be997f9f59b3
mojzesz <lukp12@gmail.com>
2015-04-28 11:34:26 +0200

    chronotime

--- app/src/main/java/com/example/lukaszpp/uselessapp2/MainActivity.java
+++ app/src/main/java/com/example/lukaszpp/uselessapp2/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.lukaszpp.uselessapp2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static long CHRONOBASE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get chronometer
+        Chronometer chronoObject = (Chronometer) findViewById(R.id.chronometer);
+      //  if(resultsActivity.CHRONOBASE == null) {
+            this.CHRONOBASE = resultsActivity.CHRONOBASE;
+       // }
+        chronoObject.setBase(CHRONOBASE);
+        chronoObject.start();
     }
 
 
@@ -35,5 +50,56 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //definiowanie stałych string
+    public final static String NAME = "com.example.lukaszpp.uselessapp2.NAME";
+    public final static String SURNAME = "com.example.lukaszpp.uselessapp2.SURNAME";
+    public final static String PLACE = "com.example.lukaszpp.uselessapp2.PLACE";
+    public final static String CHRONOTIME = "com.example.lukaszpp.uselessapp2.CHRONOTIME";
+    public final static String SEEKBARTEXT = "com.example.lukaszpp.uselessapp2.SEEKBARTEXT";
+
+
+
+    public void saveButtonClick(View view){
+
+        //get chronometer
+        Chronometer chronoObject = (Chronometer) findViewById(R.id.chronometer);
+        chronoObject.stop();
+        long chronoBase = chronoObject.getBase();
+        this.CHRONOBASE = chronoBase;
+        String chronoTime = chronoObject.getText().toString();
+
+        //get text
+        EditText textFilenameObject = (EditText) findViewById(R.id.textName);
+        String name = textFilenameObject.getText().toString();
+
+        textFilenameObject = (EditText) findViewById(R.id.textSurname);
+        String surname = textFilenameObject.getText().toString();
+
+        textFilenameObject = (EditText) findViewById(R.id.textPlace);
+        String place = textFilenameObject.getText().toString();
+
+        //get progress bar?? how?
+        SeekBar seekBarObject = (SeekBar) findViewById(R.id.seekBar);
+        String seekBarText = String.valueOf(seekBarObject.getProgress());
+
+
+        //check if empty
+
+
+        //create intent
+        Intent intent = new Intent(this, resultsActivity.class);
+
+
+        //push to intent
+        intent.putExtra(NAME, name);
+        intent.putExtra(SURNAME, surname);
+        intent.putExtra(PLACE, place);
+        intent.putExtra(SEEKBARTEXT, seekBarText);
+        intent.putExtra(CHRONOTIME, chronoTime);
+
+        //wystartowanie aktywnosci
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/lukaszpp/uselessapp2/resultsActivity.java
@@ -0,0 +1,78 @@
+package com.example.lukaszpp.uselessapp2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+
+public class resultsActivity extends ActionBarActivity {
+
+    //global
+    public static long CHRONOBASE = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        String name = getIntent().getStringExtra(MainActivity.NAME);
+        String surname = getIntent().getStringExtra(MainActivity.SURNAME);
+        String place = getIntent().getStringExtra(MainActivity.PLACE);
+        String chronoTime = getIntent().getStringExtra(MainActivity.CHRONOTIME);
+        String seekBarText = getIntent().getStringExtra(MainActivity.SEEKBARTEXT);
+
+        //chrono base
+
+        this.CHRONOBASE = MainActivity.CHRONOBASE;
+        Chronometer chronoObject = (Chronometer) findViewById(R.id.chronometer);
+        chronoObject.setBase(this.CHRONOBASE);
+        chronoObject.start();
+        
+        //set new text
+
+        //imie
+        TextView textViewObject = (TextView) findViewById(R.id.textView5);
+        textViewObject.setText(name);
+
+        //nazwisko
+        textViewObject = (TextView) findViewById(R.id.textView6);
+        textViewObject.setText(surname);
+
+        //miejsce
+        textViewObject = (TextView) findViewById(R.id.textView7);
+        textViewObject.setText(place);
+
+        //czas
+        textViewObject = (TextView) findViewById(R.id.textView8);
+        textViewObject.setText("Czas: " +chronoTime);
+
+        //seek bar
+        textViewObject = (TextView) findViewById(R.id.textView9);
+        textViewObject.setText("Lubisz uczelnie na: "+seekBarText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_results, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
